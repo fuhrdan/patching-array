a530e30793f87b9fe6d7ca8645908b0d150e5be0
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-06-16 15:53:54 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//*********************************************************
+//**330. Leetcode Patching Array                         **
+//** My original approach was to use a hash table, but   **
+//** then I went with a simple index count.  This worked **
+//** tbut it worked slowly.  -- Dan                      **
+//*********************************************************
+
+int minPatches(int* nums, int numsSize, int n) {
+    long sum = 1;
+    int result = 0;
+    int index = 0;
+    while(sum <= n)
+    {
+        if (index < numsSize && nums[index] <= sum)
+        {
+            sum += nums[index++];
+        }
+        else
+        {
+            result++;
+            sum <<= 1;
+        }
+    }
+    return result;
+}
